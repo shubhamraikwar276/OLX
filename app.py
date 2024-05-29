009b461faff28042346834a44633af60f985a3e4
shubhamraikwar276 <shubhamraikwar276@gmail.com>
2024-05-29 20:42:52 +0530

    sync 29-05-2024

--- app.py
+++ app.py
@@ -7,6 +7,19 @@ def homepage():
     return render_template("home.html")
 
 
+@app.route('/contact', methods = ["GET",'POST'])
+def contactus():
+    if request.method== 'POST':
+        name = request.form.get("name")
+        email = request.form.get("email")
+        country = request.form.get("country")
+        state = request.form.get("state")
+        message = request.form.get("message")
+        print(name,email,country,state,message)
+    else:
+        return render_template('contactus.html')
+
+
 
 
 if __name__=='__main__':
